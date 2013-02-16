a82256c8c1b7bd926d52557050b745b77abaa16e
Vlad Gerasimov <vladstudio@gmail.com>
2013-02-17 00:00:54 +0900

    added i18n

--- move-to-kirby/content/smart-submit/do-something.php
+++ move-to-kirby/content/smart-submit/do-something.php
@@ -5,9 +5,9 @@ header('Content-type: application/json');
 
 //validate using Kirby toolkit
 $errors = array();
-if (!get('name'))            	{ die('{"error":"Please enter your name."}'); }
-if (!v::email(get('email'))) 	{ die('{"error":"Please enter valid e-mail address."}'); }
-if (!get('text'))            	{ die('{"error":"Message is required."}'); }
+if (!get('name'))            	{ die('{"error":"'.(l::get('error.name-required') ?: 'Please enter your name.').'"}'); }
+if (!v::email(get('email'))) 	{ die('{"error":"'.(l::get('error.email-invalid') ?: 'Please enter valid e-mail address.').'"}'); }
+if (!get('text'))            	{ die('{"error":"'.(l::get('error.message-required') ?: 'Message is required.').'"}'); }
 
 // .....
 // do actual work here! Send email, add info to database, whatever.
---
+++ move-to-kirby/site/languages/smart-submit.en.php
@@ -0,0 +1,7 @@
+<?php 
+
+l::set('error.name-required', 'Please enter your name.');
+l::set('error.email-invalid', 'Please enter valid e-mail address.');
+l::set('error.message-required', 'Message is required.');
+l::set('smart-submit-alarm', 'Anti-spam alarm. Please try again.');
+l::set('smart-submit-missing-handler', 'Error - handler not found. Please contact web site administrator for details.');
